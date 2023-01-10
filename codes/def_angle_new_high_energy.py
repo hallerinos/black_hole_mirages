28de036f3018931451c7f3203751943a0f9546ba
Andreas Haller <andreas.haller@uni.lu>
2023-01-10 16:40:54 +0100

    .

--- codes/def_angle_new_high_energy.py
+++ codes/def_angle_new_high_energy.py
@@ -30,14 +30,14 @@ def filled_arc(center, radius, theta1, theta2, ax, color):
 
 
 mpl.rcParams['text.usetex'] = True
-mpl.rcParams['figure.figsize'] = (6.2,6.2/2)
+mpl.rcParams['figure.figsize'] = (6.2,6.2)
 plt.rc('text.latex', preamble=r'\usepackage{bm}')
 
 csvfile = 'codes/capture_high_energies.csv'
 
 r0 = 3
 
-fig, axs = plt.subplots(1, 2)
+fig, axs = plt.subplots(2, 2)
 axs = axs.ravel()
 
 lbls = 'abcdefghijklmnopqrstuvwxyz'
@@ -46,92 +46,135 @@ szes = 0.04*np.asfarray(pranges)
 arrowoffs=[[0,60,160,33],[300,20,600,93],[400,30,900,35]]
 arrowoffs=[[0,180,130,12],[0,250,400,98],[0,30,150,77],[0,20,400,65]]
 
-cols = []
 data = pd.read_csv(csvfile)
-circle = plt.Circle([0,0], 3.0/2, facecolor='black', alpha=0.2, edgecolor='none')
-axs[0].add_patch(circle)
-circle = plt.Circle([0,0], 1, facecolor='black', alpha=0.2, edgecolor='none')
-axs[0].add_patch(circle)
-circle = plt.Circle([0,0], 0.12, facecolor='black', edgecolor='none', zorder=10)
-axs[0].add_patch(circle)
-axs[0].set_aspect('equal')
+ax = axs[0]
+ax.set_aspect('equal')
+ax.plot([0,0],[-100,100], color='black', linewidth=0.5)
+ax.plot([-100,100],[0,0], color='black', linewidth=0.5)
+cmap = mpl.cm.get_cmap('viridis')
 
-axs[0].plot([0,0],[-100,100], color='black', linewidth=0.5, zorder=0)
-axs[0].plot([-100,100],[0,0], color='black', linewidth=0.5, zorder=0)
-
-nkeys = np.unique(data[data['t']==0]['E'])[:-1]
+nkeys = np.unique(data[data['t']==0]['E'])
+cols = [cmap(x) for x in np.linspace(0,1,len(nkeys))]
 for (idn,n) in enumerate(nkeys):
-    print(n)
-    data_sel = data[abs(data['E']-n)<1e-4]
+    data_sel = data[abs(data['E']-n)<1e-3]
+    idxs = data_sel[abs(data_sel['E']-n)>1e-5].index
+    if not idxs.empty:
+        maxi = idxs[0]
+        if abs(n - 0.01) < 1e-7:
+            maxi = 152
+        data_sel = data_sel.loc[:maxi]
     Xs, Ys = np.asfarray(data_sel['x']), np.asfarray(data_sel['y'])
     Xs /= data_sel['\\rho_t']
     Ys /= data_sel['\\rho_t']
-    print(Xs, Ys)
-    # exit()
 
-    p = axs[0].plot(Xs, Ys, zorder=10, color='black')
-    col = p[-1].get_color()
-    cols.append(col)
+    lbl = n.round(2)
+    if n==2:
+        p = ax.plot(Xs, Ys, zorder=999, label=f'$\\rm linearized$', linestyle='dashed', color='black')
+    else:
+        p = ax.plot(Xs, Ys, zorder=len(nkeys)-idn-1, label=f'$Em^*a^2={lbl}$', color=cols[idn])
+circle = plt.Circle([0,0], 3.0/2, facecolor='black', alpha=0.2, edgecolor='none')
+ax.add_patch(circle)
+circle = plt.Circle([0,0], 1, facecolor='black', alpha=0.2, edgecolor='none')
+ax.add_patch(circle)
+circle = plt.Circle([0,0], 0.12, facecolor='black', edgecolor='none', zorder=10)
+ax.add_patch(circle)
+ax.text(0.0125, 0.92, f"$(a)$", transform = ax.transAxes)
+ax.set_xlim(-4,+4)
+ax.set_ylim(-4,+4)
+ax.set_xlabel('$x/\\rho_t$')
+ax.set_ylabel('$y/\\rho_t$')
 
-    func = lambda x, a, b: a + b*x
+csvfile = 'codes/weak_lensing_high_energies.csv'
+data = pd.read_csv(csvfile)
+ax = axs[1]
+ax.set_aspect('equal')
+ax.plot([0,0],[-100,100], color='black', linewidth=0.5)
+ax.plot([-100,100],[0,0], color='black', linewidth=0.5)
 
-    s2 = -200
-    Xs_fit, Ys_fit = Xs[s2:], Ys[s2:]
+nkeys = np.unique(data[data['t']==0]['E'])
+for (idn,n) in enumerate(nkeys):
+    data_sel = data[abs(data['E']-n)<1e-3]
+    idxs = data_sel[abs(data_sel['E']-n)>1e-5].index
+    if not idxs.empty:
+        maxi = idxs[0]
+        if abs(n - 0.01) < 1e-7:
+            maxi = 152
+        data_sel = data_sel.loc[:maxi]
+    Xs, Ys = np.asfarray(data_sel['x']), np.asfarray(data_sel['y'])
+    Xs /= data_sel['\\rho_t']
+    Ys /= data_sel['\\rho_t']
 
-    best_fit_ab, covar = curve_fit(func, Xs_fit, Ys_fit)
-    
-    sigma_ab = np.sqrt(np.diagonal(covar))
-    a = best_fit_ab[0]
-    b = best_fit_ab[1]
-    
-    p = axs[0].plot([-100,100], a + b*np.asfarray([-100,100]), linestyle='dashed', zorder=10)
-    phi1 = 53.9
-    filled_arc([2.93,-4.1], 6.5, 0, phi1, axs[0], p[-1].get_color())
-    axs[0].text(5, 1, "$\\phi_2$", color=p[-1].get_color())
+    lbl = n.round(2)
+    if n==2:
+        p = ax.plot(Xs, Ys, zorder=999, label=f'$Em^*a^2={lbl}$', linestyle='dashed', color='black')
+    else:
+        p = ax.plot(Xs, Ys, zorder=len(nkeys)-idn-1, label=f'$Em^*a^2={lbl}$', color=cols[idn])
+circle = plt.Circle([0,0], 3.0/2, facecolor='black', alpha=0.2, edgecolor='none')
+ax.add_patch(circle)
+circle = plt.Circle([0,0], 1, facecolor='black', alpha=0.2, edgecolor='none')
+ax.add_patch(circle)
+circle = plt.Circle([0,0], 0.12, facecolor='black', edgecolor='none', zorder=10)
+ax.add_patch(circle)
+ax.text(0.0125, 0.92, f"$(b)$", transform = ax.transAxes)
+ax.set_xlim(-10,+10)
+ax.set_ylim(-10,+10)
+ax.set_xlabel('$x/\\rho_t$')
+ax.set_ylabel('$y/\\rho_t$')
 
-    s1 = 200
-    Xs_fit, Ys_fit = Xs[:s1], Ys[:s1]
-    
-    best_fit_ab, covar = curve_fit(func, Xs_fit, Ys_fit)
-    
-    sigma_ab = np.sqrt(np.diagonal(covar))
-    a = best_fit_ab[0]
-    b = best_fit_ab[1]
-    
-    p = axs[0].plot([-100,100], a + b*np.asfarray([-100,100]), linestyle='dashed', zorder=10)
-    phi2 = 17.5
-    filled_arc([2.93,-4.1], 5, 0, phi2, axs[0], p[-1].get_color())
-    filled_arc([2.93,-4.1], 2.9, 180+phi2, 180+phi1, axs[0], 'black')
-    axs[0].text(2, -6.5, "$\\varphi$", color='black')
-    axs[0].text(6, -5, "$\\phi_1$", color=p[-1].get_color())
-axs[0].text(0.0125, 0.94, f"$(a)$", transform = axs[0].transAxes)
-axs[0].set_xlim(-4,+10)
-axs[0].set_ylim(-10,+6)
-axs[0].set_xlabel('$x/r_t$')
-axs[0].set_ylabel('$y/r_t$')
-
-csvfile = 'codes/lensing_angle_high_energies_diag_chi_1_rhot_1_vF_1.csv'
+ax = axs[2]
+csvfile = 'codes/lensing_angle_high_energies_diag_chi_1_rhot_110_vF_1.csv'
 data_numeric = pd.read_csv(csvfile)
-ks = np.unique(data_numeric['k(0)'])
-for k in ks:
-    data_slice = data_numeric[data_numeric['k(0)']==k]
+enes = np.unique(data_numeric['E'])
+for (ide,e) in enumerate(enes):
+    data_slice = data_numeric[data_numeric['E']==e]
     Xs, Ys = np.asarray(data_slice[f'\\rho_p/\\rho_t']), np.asarray(data_slice[f'\\varphi'])
     order = np.argsort(Xs)
     Xs, Ys = Xs[order], Ys[order]
     enth = 1
-    axs[1].plot(Xs[::enth], Ys[::enth], zorder=1, label=f'$k_xa={k}$')
+    if e == 2:
+        ax.plot(Xs[::enth], Ys[::enth], zorder=999, color='black', linestyle='dashed')
+    else:
+        ax.plot(Xs[::enth], Ys[::enth], zorder=len(nkeys)-idn-1, color=cols[ide])
+ax.set_xscale('log')
+ax.set_yscale('log')
+ax.set_xlim(3,1e5)
+ax.set_ylim(1e-5,2)
+ax.set_xlabel('$\\rho_p/\\rho_t$')
+ax.set_ylabel('$\\varphi$')
+ax.text(0.0125, 0.92, f"$(c)$", transform = ax.transAxes)
 
-# axs[1].plot([], [], color='black', zorder=1, label='$\\rm Eq.\\ (11)$')
-# axs[1].plot([], [], linestyle='dotted', color='black', zorder=1, label='$\\rm Eq.\\ (13)$')
-
-axs[1].set_xscale('log')
-axs[1].set_yscale('log')
-axs[1].set_xlim(1,1e5)
-axs[1].set_ylim(1e-5,2)
-axs[1].set_xlabel('$\\rho_p$')
-axs[1].set_ylabel('$\\varphi$')
-axs[1].text(0.92, 0.94, f"$(b)$", transform = axs[1].transAxes)
-axs[1].legend(fancybox=False, frameon=False)
+ax = axs[3]
+csvfile = 'codes/lensing_angle_high_energies_KX_chi_1_rhot_110_vF_1.csv'
+data_numeric = pd.read_csv(csvfile)
+enes = np.unique(data_numeric['E'])
+for (ide,e) in enumerate(enes):
+    data_slice = data_numeric[data_numeric['E']==e]
+    Xs, Ys = np.asarray(data_slice[f'\\rho_p/\\rho_t']), np.asarray(data_slice[f'\\varphi'])
+    order = np.argsort(Xs)
+    Xs, Ys = Xs[order], Ys[order]
+    enth = 1
+    if e == 2:
+        ax.plot(Xs[::enth], Ys[::enth], zorder=999, color='black', linestyle='dashed')
+    else:
+        ax.plot(Xs[::enth], Ys[::enth], zorder=len(nkeys)-idn-1, color=cols[ide])
+ax.set_xscale('log')
+ax.set_yscale('log')
+ax.set_xlim(3,1e5)
+ax.set_ylim(1e-5,2)
+ax.set_xlabel('$\\rho_p/\\rho_t$')
+ax.set_ylabel('$\\varphi$')
+ax.text(0.0125, 0.92, f"$(d)$", transform = ax.transAxes)
 plt.tight_layout()
 plt.savefig('fig/deflection_angle_high_energy.pdf', bbox_inches='tight', pad_inches=0.01)
 plt.savefig('fig/deflection_angle_high_energy.png', dpi=1200, bbox_inches='tight', pad_inches=0.01)
+
+# then create a new image
+# adjust the figure size as necessary
+figsize = (6.2, 0.2)
+fig_leg = plt.figure(figsize=figsize)
+ax_leg = fig_leg.add_subplot(111)
+# add the legend from the previous axes
+ax_leg.legend(*axs[0].get_legend_handles_labels(), loc='center', ncol=4, frameon=False)
+# hide the axes frame and the x/y labels
+ax_leg.axis('off')
+fig_leg.savefig('fig/deflection_angle_high_energy_legend.pdf', bbox_inches='tight', pad_inches=0.01)
